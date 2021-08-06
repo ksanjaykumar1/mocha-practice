8b9aa10b48ecf7498cd39a36c28a558ef7d17c46
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-07 05:06:46 +0530

    myClass.js completed

--- src/myClass.js
+++ src/myClass.js
@@ -1,14 +1,54 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 class MyClass{
     constructor(){
         console.log("initiated")
+    }
+    sayHello(str){
+        console.log(str)
     }
 
     add (arg1, arg2){
         return arg1+arg2
     }
     anotherFn(arg1,arg2){
+        this.sayHello("Hello Rilke");
         return this.add(arg1,arg2)
     }
+    callback(callbackfunction){
+        // console.log(callbackfunction)
+        callbackfunction()
+    }
+
+    testPromise(){
+        return new Promise(function(resolve,reject){
+            setTimeout(() => resolve(3), 3000);
+        })
+        
+    }
+    xhrFn() {
+        return new Promise((resolve, reject) => {
+          var xhr = new XMLHttpRequest();
+          xhr.open("post", "https://echo-service-new.herokuapp.com/echo", true);
+    
+          xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+              if (xhr.status == 200) {
+                resolve(JSON.parse(xhr.responseText));
+              } else {
+                reject(xhr.status);
+              }
+            }
+          };
+          xhr.send();
+        })
+          .then(function(result) {
+            return result;
+          })
+          .catch(error => {
+            return error;
+          });
+      }
 }
+
 
 module.exports = MyClass;
